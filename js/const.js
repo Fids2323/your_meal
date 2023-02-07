df01e1d768c0eb8f7cb0d10855c3bb09a89ae68b
Dmitry Homskiy <workfids23@gmail.com>
2023-02-07 14:10:18 +0300

    fix

--- js/const.js
+++ js/const.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://night-indecisive-dart.glitch.me'
-export const PREFIX_PRODUCT='/api/product'
+export const API_URL = "https://night-indecisive-dart.glitch.me";
+export const PREFIX_PRODUCT = "/api/product";
